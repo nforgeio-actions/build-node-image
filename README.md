6f27268d3aa0e3a0a474a0e5d209bbce62cf5622
Jeff Lill <jeff@lilltek.com>
2021-05-21 09:54:09 -0700

    coding...

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# build-node-images
+# node-image-builder
 
 **INTERNAL USE ONLY:** This GitHub action is not intended for general use.  The only reason 
 why this repo is public is because GitHub requires it.
 
-Builds and publishes neonKUBE node images.
-
+Builds neonKUBE node images.
--- action.ps1
+++ action.ps1
@@ -30,6 +30,141 @@ Push-Location $ncPowershell
 . ./includes.ps1
 Pop-Location
 
-# Clear the runner state.
+# Fetch the inputs
 
-Clear-Directory $env:GITHUB_WORKSPACE
+$hostType     = Get-ActionInput "host-type"      $true
+$baseImageUri = Get-ActionInput "base-image-uri" $true
+$buildCommit  = Get-ActionInput "build-commit"   $true
+$buildLogName = Get-ActionInput "build-log"      $true
+$buildLogPath = [System.IO.Path]::Combine($env:GITHUB_WORKSPACE, $buildLogName)
+
+$env:GITHUB_WORKSPACE = "C:\Temp\NodeImage"
+#-------------------------------
+
+# Initialize the outputs
+
+Set-ActionOutput "success"   "true"
+Set-ActionOutput "build-log" $buildLogPath
+
+# Perform the operation
+
+try
+{
+    # Validate the target host type and configure the node-image command options
+
+    $targetFolder       = $env:GITHUB_WORKSPACE
+    # $publishOption      = "--publish"
+    $publishOption      = "--no-containers"
+    $hostAddressOption  = ""
+    $hostAccountOption  = ""
+    $hostPasswordOption = ""
+    $nodeAddressOption  = ""
+    $nodeNameOption     = ""
+
+    Switch ($hostType)
+    {
+        "wsl2"
+        {
+        }
+
+        "hyperv"
+        {
+            $nodeAddressOption = "--node-address=10.0.1.10"     # <-- $todo(jefflill): needs to be read from neon-assistant profile
+        }
+
+        "xenserver"
+        {
+            throw "Not implemented"
+        }
+
+        "aws"
+        {
+            throw "Not implemented"
+        }
+
+        "azure"
+        {
+            throw "Not implemented"
+        }
+
+        default
+        {
+            throw "Unknown build target: $hostType"
+        }
+    }
+
+    # Discard any neonCLOUD commits and then checkout the requested commit
+
+    Push-Cwd $ncRoot
+
+        git reset --quiet --hard
+        ThrowOnExitCode
+    
+        git fetch --quiet
+        ThrowOnExitCode
+
+        git checkout --quiet --detach $buildCommit
+        ThrowOnExitCode
+
+    Pop-Cwd
+
+    #--------------------------------------------------------------------------
+    # Build neonCLOUD (including tools) so we can use the [neon-image] tool
+
+    Write-Output ""                                                             > $buildLogPath
+    Write-Output "===========================================================" >> $buildLogPath
+    Write-Output "Building neonCLOUD (with tools)"                             >> $buildLogPath
+    Write-Output "===========================================================" >> $buildLogPath
+    Write-Output ""                                                            >> $buildLogPath
+
+    $buildScript = [System.IO.Path]::Combine($env:NC_TOOLBIN, "neoncloud-builder.ps1")
+
+    pwsh $buildScript -tools 2>&1 >> $buildLogPath
+    ThrowOnExitCode
+
+    #--------------------------------------------------------------------------
+    # Build and publish the requested node image
+
+    Write-Output ""                                                            >> $buildLogPath
+    Write-Output "===========================================================" >> $buildLogPath
+    Write-Output "Building [$hostType] node image"                             >> $buildLogPath
+    Write-Output "===========================================================" >> $buildLogPath
+    Write-Output ""                                                            >> $buildLogPath
+
+    $neonImagePath = [System.IO.Path]::Combine($env:NC_BUILD, "neon-image", "neon-image.exe")
+
+    # Remove any locally cached images
+
+    $result = Invoke-CaptureStreams "$neonImagePath prepare clean" -interleave
+    Write-Output $result.stdout >> $buildLogPath
+
+    # Prepare the node image for the target environment
+
+    $result = Invoke-CaptureStreams "$neonImagePath prepare node $hostType $targetFolder $baseImageUri $nodeAddressOption $hostAddressOption $hostAccountOption $hostPasswordOption $nodeNameOption $publishOption" -interleave
+    Write-Output $result.stdout >> $buildLogPath
+}
+catch
+{
+    Write-ActionException $_
+    Set-ActionOutput "success" "false"
+
+    # Discard any neonCLOUD commits and checkout master 
+
+    Push-Cwd $ncRoot
+
+        git reset --quiet --hard
+        ThrowOnExitCode
+    
+        git fetch --quiet
+        ThrowOnExitCode
+
+        git checkout --quiet master
+        ThrowOnExitCode
+    
+        git pull --quiet
+        ThrowOnExitCode
+
+    Pop-Cwd
+
+    exit 1
+}
